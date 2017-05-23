b3045143bd8eb0070ae6fc3aa73c3c52a15c3482
Alexis Rondeau <alexis.rondeau@gmail.com>
2017-05-23 17:49:50 +0200

    Removes /test and uses /src for test-path

--- test/blockchain_toy/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns blockchain-toy.core-test
-  (:require [clojure.test :refer :all]
-            [blockchain-toy.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
