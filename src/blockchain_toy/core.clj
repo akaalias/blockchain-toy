a492047788c59e752d06602ce0bd5040077d2122
Alexis Rondeau <alexis.rondeau@gmail.com>
2017-05-24 15:37:40 +0200

    Refactors to something more readable

--- src/blockchain_toy/core.clj
+++ src/blockchain_toy/core.clj
@@ -1,7 +1,7 @@
 (ns blockchain-toy.core
   (:require [clojure.test :refer :all]
             [digest :refer :all]))
-(with-test 
+(with-test
   (defn generate-hash [num data nonce]
     (sha-256 (str num nonce data)))
 
@@ -11,26 +11,40 @@
   (is (= (generate-hash 1 1 1) "f6e0a1e2ac41945a9aa7ff8a8aaa0cebc12a3bcc981a929ad5cf810a090e11ae"))
   (is (= (generate-hash 2 1 1) "093434a3ee9e0a010bb2c2aae06c2614dd24894062a1caf26718a01e175569b8")))
 
+(defn generate-zeros-string [z-count]
+  (clojure.string/join (repeat z-count "0")))
+
 (with-test
-  (defn make-nonce-for-zeros-finder [z-count max]
-    (fn [num data nonce]
-      (cond (> nonce max) nil
-            (= (clojure.string/join (repeat z-count "0")) (subs (generate-hash num data nonce) 0 z-count)) nonce
-            :else (recur num data (inc nonce)))))
+  (defn make-nonce-for-zeros-finder [z-count]
+    (fn
+      ([num data]
+       ((make-nonce-for-zeros-finder z-count) num data 0))
+      ([num data nonce]
+       (cond (= (generate-zeros-string z-count) (subs (generate-hash num data nonce) 0 z-count)) nonce
+             :else (recur num data (inc nonce))))))
 
-  (is (= ((make-nonce-for-zeros-finder 1 100) nil nil 100) nil))
-  (is (= ((make-nonce-for-zeros-finder 1 100) nil nil 0) 39))
-  (is (= ((make-nonce-for-zeros-finder 1 100) 1 nil 0) 25))
-  (is (= ((make-nonce-for-zeros-finder 1 100) 1 1 0) 11))
-  (is (= ((make-nonce-for-zeros-finder 1 100) "foo" "bar" 0) 20))
+  (is (= ((make-nonce-for-zeros-finder 1) nil nil) 39))
+  (is (= ((make-nonce-for-zeros-finder 1) 1 nil) 25))
+  (is (= ((make-nonce-for-zeros-finder 1) 1 1) 11))
+  (is (= ((make-nonce-for-zeros-finder 1) "foo" "bar" 0) 20))
+  (is (= ((make-nonce-for-zeros-finder 2) nil nil) 286))
+  (is (= ((make-nonce-for-zeros-finder 4) nil nil) 88484))
+  (is (= ((make-nonce-for-zeros-finder 4) 1 1) 64840))
+  (is (= ((make-nonce-for-zeros-finder 4) "foo" "bar") 42515)))
 
-  (is (= ((make-nonce-for-zeros-finder 2 1000) nil nil 0) 286))
-  
-  (is (= ((make-nonce-for-zeros-finder 4 100000) nil nil 100000) nil))
-  (is (= ((make-nonce-for-zeros-finder 4 100000) nil nil 100001) nil))
-  (is (= ((make-nonce-for-zeros-finder 4 100000) nil nil 0) 88484))
-  (is (= ((make-nonce-for-zeros-finder 4 100000) 1 1 0) 64840))
-  (is (= ((make-nonce-for-zeros-finder 4 100000) "foo" "bar" 0) 42515)))
+(def find-nonce-for-one-zero-padded-hash
+  (make-nonce-for-zeros-finder 1))
 
+(def find-nonce-for-two-zeros-padded-hash
+  (make-nonce-for-zeros-finder 2))
 
-(generate-hash "foo" "bar" 20)
+(def find-nonce-for-three-zeros-padded-hash
+  (make-nonce-for-zeros-finder 3))
+
+(def find-nonce-for-four-zeros-padded-hash
+  (make-nonce-for-zeros-finder 4))
+
+(def find-nonce-for-five-zeros-padded-hash
+  (make-nonce-for-zeros-finder 5))
+
+;; and so on...
